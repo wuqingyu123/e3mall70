661195359606ea523f62bd42a52416d303faea50
wuqingyu <wqyJava@163.com>
2017-12-22 08:53:44 +0800

    第二次修改

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,8 +1,11 @@
 package cn.e3mall.service;
 
+import cn.e3mall.common.pojo.DataResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 
 	public TbItem selectByPrimaryKey(long id);
+
+	public DataResult getItemList(int page, int rows);
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
 @Service
@@ -16,6 +23,23 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public TbItem selectByPrimaryKey(long id) {
 		return itemMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public DataResult getItemList(int page, int rows) {
+		// 1、设置分页信息
+		PageHelper.startPage(page, rows);
+		// 2、执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		// 3、取分页结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		long total = pageInfo.getTotal();
+		// 4、返回DataGridResult对象
+		DataResult result = new DataResult();
+		result.setRows(list);
+		result.setTotal(total);
+		return result;
 	}
 	
 }
